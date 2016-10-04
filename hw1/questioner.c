8ce89da7b70fee5b5876e1a84855d66b09872eb0
gabi <gabib@live.com>
2016-10-03 20:48:59 -0700

    finished questioner

--- hw1/questioner.c
+++ hw1/questioner.c
@@ -7,33 +7,48 @@
 
 int main() {
 
-int numGuessed = -2;
-int numActual = -2;
-char userName[20];
-int nameLen = strlen(userName);
-
-FILE* textFile;
-char* line = NULL;
-size_t len = 0;
-
-// get user input
-printf("What is your name?\n");
-scanf("%20s", &userName);
-
-printf("What is the magic number, %s\n", userName);
-scanf("%d", &numGuessed);
-
-// open text file for reading answers
-textFile = fopen("answers.txt", "r");
-
-// read through file and find a name matching the user
-while ((getline(&line, &len, textFile)) != -1) {
-	if (strncmp(userName, line, nameLen) == 0) {
-		numActual = 
-		printf("match: %s", line);
+	int numGuessed = -2;
+	int numActual = 12345;
+	char nameInput[20];
+	char nameFile[20];
+	int nameLen = 0;
+	
+	FILE* textFile;
+	
+	// get user input
+	printf("What is your name?\n");
+	scanf("%20s", nameInput);
+	
+	printf("What is the magic number, %s\n", nameInput);
+	scanf("%d", &numGuessed);
+	if (numGuessed == -2) {
+		printf("error getting magic number\n");
+		exit(-1);
 	}
-}
+	
+	// open text file for reading answers
+	textFile = fopen("answers.txt", "r");
+	if (!textFile) {
+		printf("Unable to open textfile answers.txt\n");
+		exit(-2);
+	}
+	
+	// read through file and find a name matching the user
+	while(fscanf(textFile, "%s %d", nameFile, &numActual) != EOF) {
+		nameLen = strlen(nameInput);
+		if (strncmp(nameInput, nameFile, nameLen) == 0) {
+			break;
+		}
+	}
+	
+	// Compare numbers
+	if (numGuessed > numActual) {
+		printf("TOO HIGH\n");
+	} else if (numGuessed < numActual) {
+		printf("TOO LOW\n");
+	} else {
+		printf("CORRECT\n");
+	}
 
-
-return 0;
+	return 0;
 }
